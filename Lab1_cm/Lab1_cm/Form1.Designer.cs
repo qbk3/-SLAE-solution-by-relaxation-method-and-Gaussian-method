56f3000b2672bab583317fd20e183e25ff03c9f2
Николай <71391450+qbk3@users.noreply.github.com>
2021-04-07 02:33:40 +0300

    Add files via upload
    Regular version without checking for many exceptions

---
+++ Lab1_cm/Lab1_cm/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Lab1_cm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(805, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Считать матрицу ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(805, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Метод релаксации";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0,0001";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1,5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ω Метод Релаксации";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Точность Решения";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 241);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(241, 280);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(259, 241);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(558, 280);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 524);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Матрица/Уравнение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 524);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ответы";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(805, 50);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Метод Гаусса";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(259, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(365, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Начальное приближение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 616);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Метод Релаксации";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab1_cm/Lab1_cm/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab1_cm
+{
+    public partial class Form1 : Form
+    {
+        public double[,] m;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //R = B - AX
+
+        public double[] Relaxation(double[,] matr) // Метод Релаксации решения СЛАУ лабараторная работа 1 задние 4,4
+        {
+            // Разделим матрицу на две матрицы, матрицу коэф. и матрицу свободных членов
+            int n = matr.GetLength(0);
+            int iter = 0;
+            double norma;
+            double[,] A = new double[matr.GetLength(0), matr.GetLength(0)];
+            double[] B = new double[matr.GetLength(0)];
+            double[] previousVariableValues = new double[matr.GetLength(0)]; //previousVariableValues -  предидущее приближение R(k -1) 
+            double[] currentVariableValues = new double[matr.GetLength(0)]; //currentVariableValues - текущие приближение R(k)
+            
+            string s = "Метод Релаксации\nПриближение:\n";
+            string s2 = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                s += "x[" + (i + 1) + "]        ";
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = matr[i, j];
+                }
+            }
+            for (int i = 0; i < n; i++)
+                B[i] = matr[i, n];
+            s += "\n";
+
+            double eps = Convert.ToDouble(textBox1.Text);
+            double w = Convert.ToDouble(textBox2.Text);
+            
+            //Задаем нулевое начальное приближение
+            for (int i = 0; i < n; i++)
+            {
+                currentVariableValues[i] = Convert.ToDouble(textBox3.Text);
+                previousVariableValues[i] = currentVariableValues[i];
+            }
+            //Реализация алгоритма 
+            do
+            {
+                iter++;
+                norma = 0;
+                double[] R = new double[n];
+                int count = 0;
+                for (int i = 0; i < n; i++) // Вывод невязок
+                {
+                    R[i] = B[i];
+                    for (int j = 0; j < n; j++)
+                    {
+                        R[i] -= A[i, j] * previousVariableValues[j];
+                        count++;
+                    }
+                    s2 += Math.Round(R[i], 7).ToString() + " || ";
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    s += Math.Round(previousVariableValues[i], 5) + "   ";
+                    previousVariableValues[i] = B[i];
+                    for (int j = 0; j < n; j++) // Построение приближеия Ri(k - 1)
+                    {
+                        if (i != j)
+                            previousVariableValues[i] = previousVariableValues[i] - A[i, j] * previousVariableValues[j]; // Записаь предидущего приближения для i != j
+                    }
+
+                    previousVariableValues[i] /= A[i, i]; // Приведем матрицу к удобному виду для решения
+                    previousVariableValues[i] = w * previousVariableValues[i] + (1 - w) * currentVariableValues[i];
+                    
+
+                    if (Math.Abs(previousVariableValues[i] - currentVariableValues[i]) > norma) // Условие окончания алгоритма, достижение задаанной точности 
+                        norma = Math.Abs(previousVariableValues[i] - currentVariableValues[i]);
+
+
+                    currentVariableValues[i] = previousVariableValues[i]; // Переход к следующей Rk, переход к следующей итерации
+
+                }
+                s += "\n";
+                s2 += "\n";
+            } while (norma > eps);
+
+            s += "\nИтераций " + iter.ToString() + "\n";
+            for (int i = 0; i < n; i++)
+            {
+                i++;
+                s += "x[" + i + "] = ";
+                i--;
+                s += previousVariableValues[i].ToString();
+                s += "\n";
+            }
+            richTextBox2.Clear();
+            richTextBox2.AppendText(s);
+            richTextBox2.AppendText("\nНевязки\n" + s2);
+
+            return previousVariableValues;
+        }
+        private double[,] GetMatrixFromFile(string fileName)
+        {
+            double[,] matrix;
+            if (File.Exists(fileName) == false)
+            {
+
+                MessageBox.Show("ERROR \nНеправельное имя файла");
+                return null;
+            }
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                string line;
+                string str = file.ReadLine();
+                string[] NM = str.Split(new char[] { ' ', 'x' }, StringSplitOptions.RemoveEmptyEntries);
+                int n = int.Parse(NM[0]);
+                int m = int.Parse(NM[1]);
+                matrix = new double[n, m];
+                for (int i = 0; (i < n) && ((line = file.ReadLine()) != null); i++)
+                {
+                    char[] delimiterChars = { ' ', '\t' };
+                    string[] numbers = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                    int j = 0;
+                    foreach (string numString in numbers)
+                    {
+                        double x;
+                        bool CanConvert = double.TryParse(numString, out x);
+                        if (CanConvert)
+                        {
+                            matrix[i, j] = x;
+                            j++;
+                        }
+                    }
+                }
+            }
+            int k = 0;
+            for(int i = 0; i < matrix.GetLength(0); i++)
+                for(int j = 0; j <matrix.GetLength(1); j++ )
+                {
+                    if (matrix[i, j] == 0) k++;
+                    if(k == matrix.GetLength(0) * matrix.GetLength(1))
+                    {
+                        MessageBox.Show("Расширенная матрица нулевая\nВыбирите другой файл");
+                        break;
+                    }
+                }
+
+            MessageBox.Show("Ввод матрицы завершен");
+            m = matrix;
+            return matrix;
+        }
+        private double GaussMethod(double[,] matr)
+        {
+            int n = matr.GetLength(0);
+            double[,] A = new double[matr.GetLength(0), matr.GetLength(0)];
+            double[] B = new double[matr.GetLength(0)];
+            double s;
+            double[] x = new double[n];
+
+            for (int i = 0; i < n; i++) x[i] = 0; //Массив ответов
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = matr[i, j];
+                }
+            } //Перезапись массивов 
+            for (int i = 0; i < n; i++)
+                B[i] = matr[i, n];
+
+            double max;
+            int k, index;
+            double eps = 0.00001;  // точность
+            k = 0;
+            string l = "";
+            while (k < n)
+            {
+                // Поиск строки с максимальным a[i][k]
+                max = Math.Abs(A[k,k]);
+                index = k;
+                for (int i = k + 1; i < n; i++)
+                {
+                    if (Math.Abs(A[i, k]) > max)
+                    {
+                        max = Math.Abs(A[i, k]);
+                        index = i;
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        l += Math.Round(A[i, j], 5).ToString() + " || ";
+                    }
+                    l += Math.Round(B[i], 5).ToString() + "\n";
+                }
+                //Перестановка строк
+                if (max < eps)
+                {
+                    // нет ненулевых диагональных элементов
+                    richTextBox2.Clear();
+                    richTextBox2.AppendText("Решение получить невозможно \n");
+                }
+
+                double temp = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    temp = A[k,j];
+                    A[k,j] = A[index,j];
+                    A[index,j] = temp;
+                }
+                temp = B[k];
+                B[k] = B[index];
+                B[index] = temp;
+                // Нормализация уравнений
+                for (int i = k; i < n; i++)
+                {
+                    temp = A[i,k];
+                    if (Math.Abs(temp) < eps) continue; // для нулевого коэффициента пропустить
+                    for (int j = 0; j < n; j++)
+                        A[i,j] = A[i,j] / temp;
+                    B[i] = B[i] / temp;
+                    if (i == k) continue; // уравнение не вычитать само из себя
+                    for (int j = 0; j < n; j++)
+                        A[i,j] = A[i,j] - A[k,j];
+                    B[i] = B[i] - B[k];
+                }
+                k++;
+                l += "================================\n";
+            }
+            //if(B[n] !=0)
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    l += Math.Round(A[i, j], 5).ToString() + " || ";
+                }
+                l += Math.Round(B[i], 5).ToString() + "\n";
+            }
+            // обратная подстановка
+            for (k = n - 1; k >= 0; k--)
+            {
+                x[k] = B[k];
+                for (int i = 0; i < k; i++)
+                    B[i] = B[i] - A[i,k] * x[k];
+            }
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                flag = false;
+                for (int j = 0; j < n; j++)
+                {
+                    if (A[i, j] == 0)
+                        flag = false;
+                    else
+                    {
+                        flag = true;
+                        break;
+                    } 
+                }
+                if (!flag)
+                {
+                    if (B[i] == 0) break;
+                    else flag = true;
+                }
+            }
+            string m = "Метод Гаусса\n";
+            if (!flag) m += "Ситсема имеет множество решений\n Одно из них\n";
+            for (int i = 0; i < n; i++)
+            {
+                i++;
+                m += "x[" + i + "] = ";
+                i--;
+                m += x[i].ToString();
+                m += "\n";
+            }//Вывод на экран 
+            richTextBox2.Clear();
+            richTextBox2.AppendText(m);
+            richTextBox2.AppendText(l);
+            return 1;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Файлы txt|*.txt";
+            double[,] matrix;
+            string s = "";
+            richTextBox1.Clear();
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                matrix = GetMatrixFromFile(OPF.FileName);
+                for (int i = 0; i < matrix.GetLength(0); i++, Console.WriteLine())
+                {
+
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        s += "  " + matrix[i, j] + " ";
+                    s += "\n";
+                }
+                richTextBox1.AppendText(s);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var test = Relaxation(m);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var test = GaussMethod(m);
+        }
+    }
+}
+// выводить вектор невязки 
+// исправить выраждегые матрицы 
+//
